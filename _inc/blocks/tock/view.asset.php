668994c561987f00d855561075644439682fe6b2
Luiz Tiago Oliveira <luiztiago@gmail.com>
2025-09-25 20:32:56 -0300

    Squashed 'jetpack-15.0/' content from commit 71667396a6
    git-subtree-dir: jetpack-15.0
    git-subtree-split: 71667396a61e6018111304f6bd4f464d0379a155

---
+++ _inc/blocks/tock/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '55855df5982d726811a3');
---
+++ jetpack_vendor/automattic/jetpack-forms/dist/modules/file-field/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity', 'wp-polyfill'), 'version' => 'c30f8bc3094eb813ac0d', 'type' => 'module');
